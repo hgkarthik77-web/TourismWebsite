c7b3fdfebae619e8a82c81efc36cf47d635670ab
Admin <Admin@HARSHSOFTWARE>
2026-02-19 12:56:24 +0530

    Add project files.

---
+++ TourismWebsite/Controllers/HomeController1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TourismWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Packages()
+        {
+            return View();
+        }
+
+        public IActionResult Gallery()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Trips()
+        {
+            return View();
+        }
+        public IActionResult Cultural()
+        {
+            return View();
+        }
+
+
+        public IActionResult Visas()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
